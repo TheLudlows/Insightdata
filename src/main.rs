3c4a40a68a386ba420adb2209352072776b84536
l00817341 <liuchao321@huawei.com>
2024-04-11 20:32:03 +0800

    init

---
+++ src/main.rs
@@ -0,0 +1,20 @@
+use sqlparser::ast::Statement;
+use sqlparser::dialect::PostgreSqlDialect;
+use sqlparser::parser::{Parser, ParserError};
+use anyhow::Result;
+fn main() -> Result<()> {
+    let res = parse("select * from tablea where a = 1; select *from tableb");
+    if res.is_err() {
+        println!("{:?}", res.err())
+    } else {
+        for stat in res.unwrap() {
+            stat.
+            println!("{}", stat)
+        }
+    }
+    Ok(())
+}
+pub fn parse(sql: impl Into<String>) -> Result<Vec<Statement>, ParserError> {
+    let dialect = PostgreSqlDialect{};
+    Parser::parse_sql(&dialect, sql.into())
+}
