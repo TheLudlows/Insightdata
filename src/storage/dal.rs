73d8dea67c2576f465229d541fd0599247b13866
four <ai-l@outlook.com>
2024-07-16 22:42:27 +0800

    init

--- src/storage/dal.rs
+++ src/storage/dal.rs
@@ -1,44 +1,32 @@
 
 use anyhow::Result;
+use bytes::Bytes;
 use opendal::services::{Fs};
 use opendal::{Builder, Operator};
 
-#[tokio::test]
-async fn main() -> Result<()> {
+pub async fn write_to(path: &str, buf: Bytes) -> Result<()> {
     // Create fs backend builder.
     let mut builder = Fs::default();
     // Set the root for fs, all operations will happen under this root.
     //
     // NOTE: the root must be absolute path.
     builder.root("d:/test_data");
-    let acc =  builder.build()?;
     // `Accessor` provides the low level APIs, we will use `Operator` normally.
     let op: Operator = Operator::new(builder)?.finish();
-    op.write("abc", "abc").await?;
-    op.write_with("abc", "123").append(true).await?;
-    let r = op.read_with("abc").range(0..123).await?;
-    println!("{:?}", r);
+    op.write(path, buf).await?;
     Ok(())
 }
 
-#[test]
-fn test() {
-    let num1 = vec![1, 2, 3, 4, 5];
-    let num2 = vec![4, 5, 6, 7, 8];
-    let result = intersection(&num1, &num2);
-    assert_eq!(result, vec![4, 5]);
-}
-
-fn intersection(n1: &[i32], n2: &[i32]) -> Vec<i32> {
-    //n1.iter().filter(|&x| n2.contains(x)).cloned().collect()
-    /*let mut result = n1.to_vec();
-    result.retain(|x| n2.contains(x));
-    result*/
-
-    n1.iter().skip_while(|&x| !n2.contains(x)).cloned().collect()
-   /* let mut result = n1.to_vec();
-    result.extend(n2);
-    result*/
+pub async fn read(path: &str) -> Result<Vec<u8>> {
+    // Create fs backend builder.
+    let mut builder = Fs::default();
+    // Set the root for fs, all operations will happen under this root.
+    //
+    // NOTE: the root must be absolute path.
+    builder.root("d:/test_data");
+    let op: Operator = Operator::new(builder)?.finish();
+    let vec = op.read(path).await?;
+    Ok(vec)
 }
 
 
--- src/storage/parquet_read.rs
+++ src/storage/parquet_read.rs
@@ -1,23 +1,28 @@
+use std::sync::Arc;
 
-use std::fs::File;
-use parquet::column::page::Page;
-use parquet::file::reader::{FileReader, SerializedFileReader};
+use anyhow::Result;
+use arrow::array::{Int32Array, RecordBatch, StringArray};
+use arrow::datatypes::{DataType, Field, Schema};
+use bytes::Bytes;
+use parquet::arrow::arrow_reader::{ArrowReaderBuilder, ArrowReaderOptions, ParquetRecordBatchReaderBuilder};
+use parquet::arrow::ArrowWriter;
 
-#[test]
-fn main() {
-    // 打开Parquet文件
-    let file = File::open("example.parquet").unwrap();
-    let reader = SerializedFileReader::new(file).unwrap();
+use crate::storage::dal::{read, write_to};
 
+#[tokio::test]
+async fn main() -> Result<()> {
+    let buf = read("test_pq").await?;
+   let builder =  ParquetRecordBatchReaderBuilder::try_new();
+    ArrowReaderOptions::new().rea
     // 获取文件元数据
-    let metadata = reader.metadata();
-    let file_metadata = metadata.file_metadata();
+    /* let metadata = reader.metadata();
+     let file_metadata = metadata.file_metadata();
 
-    println!("Parquet Version: {}", file_metadata.version());
-    println!("Created By: {}", file_metadata.created_by().unwrap_or("Unknown"));
+     println!("Parquet Version: {}", file_metadata.version());
+     println!("Created By: {}", file_metadata.created_by().unwrap_or("Unknown"));*/
 
     // 遍历所有行组
-    for row_group in 0..metadata.num_row_groups() {
+    /*for row_group in 0..metadata.num_row_groups() {
         let row_group_reader = reader.get_row_group(row_group).unwrap();
         let row_group_metadata = row_group_reader.metadata();
 
@@ -44,17 +49,43 @@ fn main() {
                         println!("    Data Page:");
                         println!("      Num Values: {}", num_values);
                         println!("      Encoding: {:?}", encoding);
-                    },
+                    }
                     Page::DictionaryPage { buf, num_values, encoding, .. } => {
                         println!("    Dictionary Page:");
                         println!("      Num Values: {}", num_values);
                         println!("      Encoding: {:?}", encoding);
-                    },
+                    }
                     _ => {
                         println!("    Other Page Type");
                     }
                 }
             }
         }
+    }*/
+}
+#[tokio::test]
+async fn test_write() -> Result<()> {
+    // 创建Schema
+    let schema = Arc::new(
+        Schema::new(vec![
+            Field::new("id", DataType::Int32, true),
+            Field::new("name", DataType::Utf8, true),
+        ])
+    );
+
+    // 创建RecordBatch
+    let id_array = Int32Array::from_iter_values([1, 2, 3]);
+    let name_array = StringArray::from_iter_values(&["Alice", "Bob", "Charlie"]);
+    let batch = RecordBatch::try_new(
+        schema.clone(),
+        vec![Arc::new(id_array), Arc::new(name_array)],
+    )?;
+    let mut buffer = Vec::new();
+    {
+        let mut writer = ArrowWriter::try_new(&mut buffer, batch.schema(), None)?;
+        writer.write(&batch)?;
     }
+
+    write_to("test_pq", Bytes::from(buffer)).await?;
+    Ok(())
 }
